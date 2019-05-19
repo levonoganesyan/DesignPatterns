463cdbf5453d1f0ba548fb736fcd3fc7c4536de8
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-05-20 01:27:19 +0400

    Added operations

--- DesignPatterns/Strategy.h
+++ DesignPatterns/Strategy.h
@@ -5,5 +5,31 @@
 template <typename T>
 class Operation
 {
-    std::function<T(T, T)> m_method;
+    std::function<T(const T&, const T&)> m_method;
+public:
+    void setMethod(const std::function<T(const T&, const T&)>& method)
+    {
+        m_method = method;
+    }
+    T Execute(const T& a, const T& b)
+    {
+        return m_method(a, b);
+    }
 };
+
+int plus(int a, int b)
+{
+    return a + b;
+}
+
+int minus(int a, int b)
+{
+    return a - b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+
