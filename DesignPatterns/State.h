a077ee097a6228fb30be8c48f2fd9f9a76e5d7c7
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-06-04 17:39:41 +0400

    Added State and Solid State

--- DesignPatterns/State.h
+++ DesignPatterns/State.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include<string>
+
+class StateContext;
+
 class State
 {
-
+    virtual std::string GetName() = 0;
+    virtual void freeze(StateContext*) = 0;
+    virtual void heat(StateContext*) = 0;
 };
+
+class SolidState
+{
+private:
+    std::string name;
+public:
+    SolidState() : name("solid") {}
+};
+
+
+
+
+
