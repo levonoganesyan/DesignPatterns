fc6324b65fac1d4aaa528de88e97aebc6c14a620
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-06-05 17:47:26 +0400

    Implementing State

--- DesignPatterns/State.h
+++ DesignPatterns/State.h
@@ -1,8 +1,57 @@
 #pragma once
 
 #include<string>
+#include<iostream>
 
-class StateContext;
+class State;
+
+class StateContext
+{
+private:
+    State* state;
+    StateContext(State* state)
+        : state(state) {}
+
+    void Freeze()
+    {
+        state->Freeze(this);
+    }
+    void Heat()
+    {
+        state->Heat(this);
+    }
+
+    void SetState(State* s)
+    {
+        state = s;
+    }    
+    State* GetState()
+    {
+        return state;
+    }
+
+    ~StateContext()
+    {
+        delete state;
+    }
+};
+
+class SolidState : public State
+{
+public:
+    SolidState() : State("solid") {}
+    virtual void Freeze(StateContext* state)
+    {
+        std::cout << "Nothing happened" << std::endl;
+    }
+    virtual void Heat(StateContext* state)
+    {
+        state.set
+    }
+};
+
+
+
 
 class State
 {
@@ -17,16 +66,5 @@ public:
     virtual void Freeze(StateContext*) = 0;
     virtual void Heat(StateContext*) = 0;
 };
-
-class SolidState : public State
-{
-public:
-    SolidState() : State("solid") {}
-    virtual void Freeze(StateContext* state);
-    virtual void Heat(StateContext*) = 0;
-};
-
-
-
 
 
