a4856f5654c993d1ddd3a4421414978eb582ed8d
levonoganesyan <levon.oganesyan.am@gmail.com>
2019-06-11 18:40:28 +0400

    Finishing TemplateMethod

--- DesignPatterns/TemplateMethod.h
+++ DesignPatterns/TemplateMethod.h
@@ -1,21 +1,23 @@
 #pragma once
 
+#include<iostream>
+#include<cstdlib>
 
 class GameObject
 {
 protected:
     int players_count;
-public:
     virtual void InitializeGame() = 0;
     virtual bool EndOfGame() = 0;
     virtual void MakeMove(int player_id) = 0;
-    virtual void PrintWinner() = 0;
+    virtual void PrintWinner(int player_id) = 0;
+public:
     virtual void Play(int players)
     {
         players_count = players;
         InitializeGame();
 
-        int player_id = 0;
+        int player_id = -1;
         while (!EndOfGame())
         {
             MakeMove(player_id);
@@ -27,15 +29,58 @@ public:
 };
 
 
-class Monopoly
+class Monopoly : public GameObject
 {
     virtual void InitializeGame()
     {
         std::cout << "Initializing Monopoly" << std::endl;
     }
-    virtual bool EndOfGame() = 0;
-    virtual void MakeMove(int player_id) = 0;
-    virtual void PrintWinner() = 0;
+    virtual bool EndOfGame()
+    {
+        return rand() % 10;
+    }
+    virtual void MakeMove(int player_id)
+    {
+        std::cout << "Player " << player_id << " make a move" << std::endl;
+    }
+    virtual void PrintWinner(int player_id)
+    {
+        std::cout << "Player " << player_id << " is won!" << std::endl;
+    }
 };
+
+
+class Chess : public GameObject
+{
+    virtual void InitializeGame()
+    {
+        std::cout << "Initializing Chess" << std::endl;
+    }
+    virtual bool EndOfGame()
+    {
+        return rand() % 10;
+    }
+    virtual void MakeMove(int player_id)
+    {
+        std::cout << "Player " << player_id << " make a move" << std::endl;
+    }
+    virtual void PrintWinner(int player_id)
+    {
+        std::cout << "Player " << player_id << " is won!" << std::endl;
+    }
+};
+
+
+
+void Test()
+{
+    GameObject* game1 = new Chess();
+    game1->Play(2);
+
+
+    GameObject* game2 = new Monopoly();
+    game2->Play(10);
+
+}
 
 
